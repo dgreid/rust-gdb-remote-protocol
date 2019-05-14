5c9c38e39c4f3f0f2c121fa498d5174e255c5711
Dylan Reid <dgreid@chromium.org>
2019-05-13 19:10:18 -0700

    add a stdio example

---
+++ examples/stdio.rs
@@ -0,0 +1,28 @@
+extern crate env_logger;
+extern crate gdb_remote_protocol;
+
+use gdb_remote_protocol::{process_packets_from, Error, Handler, ProcessType, StopReason};
+use std::net::TcpListener;
+
+struct NoopHandler;
+
+impl Handler for NoopHandler {
+    fn attached(&self, _pid: Option<u64>) -> Result<ProcessType, Error> {
+        Ok(ProcessType::Created)
+    }
+
+    fn halt_reason(&self) -> Result<StopReason, Error> {
+        Ok(StopReason::Signal(5))
+    }
+}
+
+#[cfg_attr(test, allow(dead_code))]
+fn main() {
+    drop(env_logger::init());
+    let h = NoopHandler;
+    let stdin_handle = std::io::stdin();
+    let stdin = stdin_handle.lock();
+    let stdout_handle = std::io::stdout();
+    let stdout = stdout_handle.lock();
+    process_packets_from(stdin, stdout, h);
+}
